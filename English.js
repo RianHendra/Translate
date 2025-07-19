43e60d4f64b88a43275b5a2fb011be861a09e768
RianHendra <rianhendra.id@gmail.com>
2025-07-19 16:29:41 +0800

    sds

---
+++ English.js
@@ -0,0 +1,38 @@
+{
+    /*=================Login====================*/
+    "Login",
+    "No. Whatsapp",
+    "Enter No. Whatsapp",
+    "Enter",
+    "Don't have an account? Register",
+    /*=================Register====================*/
+    "Register",
+    "Full Name",
+    "Enter Full Name",
+    "No. Whatsapp",
+    "Enter No. Whatsapp",
+    "Referral Code",
+    "Enter Referral Code",
+    /*=================OTP====================*/
+    "OTP Code",
+    "Enter",
+    "The OTP code has been sent via WhatsApp. Please check your WhatsApp immediately."
+    /*=================Home====================*/
+    "Book Via Travel for my trip!",
+    "Pickup City",
+    "Destination City",
+    "Date",
+    "Passenger",
+    "Search Travel",
+    /*=================Ads====================*/
+    "Advertise with our partners",
+    "via ViaAds running ads",
+    "View advertising offers"
+    /*=================Transaction | Selesai ====================*/
+    "Process",
+    "Transaction ID",
+    "July 2, 2025",
+    "Paid",
+    "Unpaid",
+    "Pending",
+}
